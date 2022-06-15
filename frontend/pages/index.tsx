771dd500bbab33cb1e96916ba9f0c03eb03d4e0b
abdealijaroli <abdealiburu03@gmail.com>
2022-06-15 10:53:01 +0530

    home page ui

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,16 +1,47 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import { useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import {
+  Center,
+  TextInput,
+  Stack,
+  Button,
+  List,
+  ThemeIcon,
+  Checkbox,
+} from "@mantine/core";
+import { CircleCheck, CircleDashed } from "tabler-icons-react";
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.main}>
-      <h1>
-         Track your tasks and meet your deadlines.
-      </h1>
-    </div>
-  )
-}
+  const [value, setValue] = useState<string>("");
 
-export default Home
+  return (
+    <Center>
+      <Stack>
+        <h1>Track your tasks and meet your deadlines.</h1>
+        <h2>Todo:</h2>
+        <TextInput
+          placeholder="What do you want to accomplish today?"
+          value={value}
+          onChange={(e) => setValue(e.currentTarget.value)}
+        />
+        <Button size="sm">Add</Button>
+
+        <List spacing="xs" size="sm" center>
+          <h4>Clone or download repository from GitHub</h4>
+          <h4>Install dependencies with yarn</h4>
+          <h4>
+            To start development server run npm start command
+          </h4>
+          <h4>Download
+            Run tests to make sure your changes do not break the build
+          </h4>
+          <h4>Submit a pull request once you are done</h4>
+        </List>
+      </Stack>
+    </Center>
+  );
+};
+
+export default Home;
---
+++ frontend/types.ts
@@ -0,0 +1,4 @@
+export default interface formValueTypes {
+   value: string,
+   setValue: string
+}
